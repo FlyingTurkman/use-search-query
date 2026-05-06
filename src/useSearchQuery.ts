e82f7c6ee81e65e7b4e96fdf8a5d2732b7a9f069
Sharkman <dytdenizarli@hotmail.com>
2026-05-07 01:58:33 +0300

    first commit

---
+++ src/useSearchQuery.ts
@@ -0,0 +1,41 @@
+'use client'
+
+import { useRouter, useSearchParams } from 'next/navigation'
+import { useCallback } from 'react'
+
+
+
+
+export function useSearchQuery() {
+    
+    const searchParams = useSearchParams()
+    const router = useRouter()
+
+    const setSearchParams = useCallback((updates: Record<string, string | number | undefined | null>) => {
+        const params = new URLSearchParams(searchParams.toString())
+
+        Object.entries(updates).forEach(([key, value]) => {
+            if (value  === undefined || value === null || value.toString().trim() == '') {
+                params.delete(key)
+            } else {
+                params.set(key, String(value))
+            }
+        })
+
+        const queryString = params.toString()
+
+        return queryString
+    }, [searchParams])
+
+
+    const routeToSearchParams = useCallback((pathname: string, updates: Record<string, string | number | undefined | null>, scroll?: boolean) => {
+        const queryString = setSearchParams(updates)
+
+        router.replace(`${pathname}?${queryString}`, { scroll: scroll ? scroll : false})
+    }, [router, setSearchParams])
+
+    return {
+        setSearchParams,
+        routeToSearchParams
+    }
+}
